c71ecbc24b6bc80c2ec7c76fd0818486fde0b9c0
Todd Resudek <toddresudek@gmail.com>
2020-08-03 20:23:51 -0600

    bump release version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Devito.MixProject do
   def project do
     [
       app: :devito,
-      version: "1.1.1",
+      version: "1.1.0",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
